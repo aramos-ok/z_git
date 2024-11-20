16813aa1a43d090c5f875627fb103b8622cfcc53
aramos <agramos@minsait.com>
2024-11-20 13:16:52 +0100

    prueba cherry-pick

--- file02.sql
+++ file02.sql
@@ -3,5 +3,8 @@ select 'día 2024-11-20'
 select 'linea agregada'
 ;
 select 'linea agregada 2024-11-20 13:16';
+<<<<<<< HEAD
 
 select 'linea agregada 2024-11-20 13:18';
+=======
+>>>>>>> f4c8cda (agrego línea)
