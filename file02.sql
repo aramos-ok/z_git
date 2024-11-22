96cf2dc125170b0a46679e443c3719ac006c8548
aramos <agramos@minsait.com>
2024-11-22 10:23:12 +0100

    resolver conflicto

--- file02.sql
+++ file02.sql
@@ -3,8 +3,5 @@ select 'día 2024-11-20'
 select 'linea agregada'
 ;
 select 'linea agregada 2024-11-20 13:16';
-<<<<<<< HEAD
-
-select 'linea agregada 2024-11-20 13:18';
-=======
->>>>>>> f4c8cda (agrego línea)
+;
+select 'rebase feature'
